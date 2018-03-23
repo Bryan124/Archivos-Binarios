7ca8565cf18221331e6bdae9927da40454f3e96f
Bryan124 <bryan124wrights@gmail.com>
2018-03-23 07:27:28 -0600

    Archivos Binarios
    Ejercicio de Herramienta

--- ArchivoBinarioHerramientas/main.cpp
+++ ArchivoBinarioHerramientas/main.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 int main() {
 	int opcion;
-	//Cada vez que se ejecuta genera otros 100 espacios vacios, para evitar esto se puede comentar la funcion creararchivo();
-	creararchivo();
+	//Cada vez que se ejecuta genera otros 10 espacios vacios, para evitar esto se puede comentar la funcion creararchivo();
 	do {
 		cout << "* * * * * M E N U * * * * *" << endl;
 		cout << "1. Imprimir Registro de Herramienta" << endl;
 		cout << "2. Registrar Herramienta" << endl;
 		cout << "3. Modificar Herramienta" << endl;
 		cout << "4. Eliminar Herramienta" << endl;
-		cout << "5. Salir" << endl;
+		cout << "5. Crear Registro de Herramientas" << endl;
+		cout << "6. Salir" << endl;
 		cin >> opcion;
 		switch (opcion) {
 		case 1: {
@@ -37,7 +37,11 @@ int main() {
 			system("pause>NUL");
 			break;
 		}
+		case 5: {
+			cout << "Si selecciono esta opcion con anteoridad se generaran otros 100 espacios en el registro!!!" << endl;
+			creararchivo();
 		}
-	} while (opcion != 5);
+		}
+	} while (opcion != 6);
 	system("pause>NUL");
 }
